947a538fa23da259c18fc8941c19179528da170b
Ryan <ryanfkf@gmail.com>
2020-02-27 21:16:38 -0500

    added support for monitoring cache misses

--- pmu.c
+++ pmu.c
@@ -12,7 +12,10 @@
 #include <stdint.h>
 #include <inttypes.h>
 
+// func prototypes
+void do_something();
 
+// Struct def
 struct read_format {
 	uint64_t nr;
 	struct {
@@ -21,12 +24,14 @@ struct read_format {
 	} values[];
 };
 
+
+
 int main(int agrc, char **argv) {
 
 	struct perf_event_attr pe;
-	int fd1, fd2;
-	uint64_t id1, id2;
-	uint64_t val1, val2;
+	int fd1, fd2, fd3;
+	uint64_t id1, id2, id3;
+	uint64_t val1, val2, val3;
 	char buf[4096];
 	struct read_format* rf = (struct read_format*) buf;
 	int i;
@@ -38,12 +43,13 @@ int main(int agrc, char **argv) {
 	pe.size = sizeof(struct perf_event_attr);
 	pe.config = PERF_COUNT_HW_CPU_CYCLES;
 	pe.disabled = 1;
-	pe.exclude_kernel = 1;
-	pe.exclude_hv = 1;
+//	pe.exclude_kernel = 1; /* This will exclude events that happen in the kernel  */
+	pe.exclude_hv = 1; // This excludes the events in the hypervisor - not running a VM
 	pe.read_format = PERF_FORMAT_GROUP | PERF_FORMAT_ID;
 	fd1 = syscall(__NR_perf_event_open, &pe, 0, -1, -1, 0);
-
-	ioctl(fd1, PERF_EVENT_IOC_ID, &id1);
+        
+	ioctl(fd1, PERF_EVENT_IOC_ID, &id1); // returns the perf_event id value for the given event file
+       					     // the result is saved inside of id*	
 
 	// Software Page Faults
 	memset(&pe, 0, sizeof(struct perf_event_attr));
@@ -51,17 +57,33 @@ int main(int agrc, char **argv) {
 	pe.size = sizeof(struct perf_event_attr);
 	pe.config = PERF_COUNT_SW_PAGE_FAULTS;
 	pe.disabled = 1;
-	pe.exclude_kernel = 1;
-	pe.exclude_hv = 1;
+//	pe.exclude_kernel = 1; /* This will execute events that happen in the kernel  */
+	pe.exclude_hv = 1; // this excludes the events that happen in the hypervisor - not running a VM
 	pe.read_format = PERF_FORMAT_GROUP | PERF_FORMAT_ID;
-	fd2 = syscall(__NR_perf_event_open, &pe, 0, -1, fd1 /*!!!*/, 0);
+	fd2 = syscall(__NR_perf_event_open, &pe, 0, -1, fd1, 0);
 
 	ioctl(fd2, PERF_EVENT_IOC_ID, &id2);
+
+        // Something
+        memset(&pe, 0, sizeof(struct perf_event_attr));
+        pe.type = PERF_TYPE_HARDWARE;
+        pe.size = sizeof(struct perf_event_attr);
+        pe.config = PERF_COUNT_HW_CACHE_MISSES;
+        pe.disabled = 1;
+//      pe.exclude_kernel = 1;
+        pe.exclude_hv = 1;
+        pe.read_format = PERF_FORMAT_GROUP | PERF_FORMAT_ID;
+	fd3 = syscall(__NR_perf_event_open, &pe, 0, -1, fd1, 0);
 	
-	// PMU test
+        ioctl(fd3, PERF_EVENT_IOC_ID, &id3);
+
+	//sleep(20);
+        
+        // PMU test
 	ioctl(fd1, PERF_EVENT_IOC_RESET, PERF_IOC_FLAG_GROUP);
 	ioctl(fd1, PERF_EVENT_IOC_ENABLE, PERF_IOC_FLAG_GROUP);
-	do_something();
+	sleep(10);
+        do_something();
 	ioctl(fd1, PERF_EVENT_IOC_DISABLE, PERF_IOC_FLAG_GROUP);
 
 	// Read from file descriptor
@@ -69,13 +91,19 @@ int main(int agrc, char **argv) {
 	for (i = 0; i < rf->nr; i++) {
 		if (rf->values[i].id == id1) {
 			val1 = rf->values[i].value;
-		} else if (rf->values[i].id == id2) {
+		} 
+                else if (rf->values[i].id == id2) {
 			val2 = rf->values[i].value;
 		}
+                else if (rf->values[i].id == id3) {
+                        val3 = rf->values[i].value; 
+                }
+
 	}
 
 	printf("cpu cycles: %"PRIu64"\n", val1);
 	printf("page faults: %"PRIu64"\n", val2);
+        printf("cache misses: %"PRIu64"\n", val3);
 
 
 	return(0);
@@ -85,8 +113,8 @@ void do_something() {
 	int i;
 	char* ptr;
 
-	ptr = malloc(100*1024*1024);
-	for (i = 0; i < 100*1024*1024; i++) {
+	ptr = malloc(10*1024*1024);
+	for (i = 0; i < 10*1024*1024; i++) {
 		ptr[i] = (char) (i & 0xff); // pagefault
 	}
 	free(ptr);
