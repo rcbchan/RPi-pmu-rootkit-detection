6061e7e3663f096bf137c7461cb9664cb31b463b
Ryan <ryanfkf@gmail.com>
2020-04-27 22:41:00 -0400

    updated hpc.c

--- hpc.c
+++ hpc.c
@@ -15,7 +15,11 @@
 void turnon_measuring(int fd);
 void turnoff_measuring(int fd);
 
+// The following are prototypes for each HPC 
+void m_counthwinstruct();
+void m_hwmisses();
 
+/* Wrapper for perf_event_open */
 static long perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
     int cpu, int group_fd, unsigned long flags)
 {
@@ -28,9 +32,31 @@ static long perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
 
 int main(int argc, char **argv)
 {
+  m_counthwinstruct();
+  m_hwmisses();
+}
+
+
+
+
+/* Measuring functions */
+void turnon_measuring(int fd) {
+  ioctl(fd, PERF_EVENT_IOC_RESET, 0);
+  ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
+}
+
+void turnoff_measuring(int fd) {
+  ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
+}
+
+
+
+
+/* Functions for HPC */
+void m_counthwinstruct() {
   struct perf_event_attr pe;
   long long val1, val2, val3, val4;
-  int fd1, fd2, fd3, fd4;
+  int fd1, fd2, fd3, fd4; 
 
   memset(&pe, 0, sizeof(struct perf_event_attr));
   pe.type = PERF_TYPE_HARDWARE;
@@ -40,7 +66,8 @@ int main(int argc, char **argv)
   pe.exclude_kernel = 1;
   pe.exclude_hv = 1;
 
-  // have to create
+  // have to create a file descriptor for each core 
+  // Raspberry Pi 3B+
   fd1 = perf_event_open(&pe, -1, 0, -1, 0);
   fd2 = perf_event_open(&pe, -1, 1, -1, 0);
   fd3 = perf_event_open(&pe, -1, 2, -1, 0);
@@ -77,10 +104,11 @@ int main(int argc, char **argv)
   read(fd3, &val3, sizeof(long long));
   read(fd3, &val4, sizeof(long long));
 
-  printf("Used %lld instructions\n", val1);
-  printf("Used %lld instructions\n", val2);
-  printf("Used %lld instructions\n", val3);
-  printf("Used %lld instructions\n", val4);
+  printf("HW Instruction Count:\n");
+  printf("\tCPU0: Used %lld instructions\n", val1);
+  printf("\tCPU1: Used %lld instructions\n", val2);
+  printf("\tCPU2: Used %lld instructions\n", val3);
+  printf("\tCPU3: Used %lld instructions\n", val4);
   
   close(fd1);
   close(fd2);
@@ -88,11 +116,101 @@ int main(int argc, char **argv)
   close(fd4);
 }
 
-void turnon_measuring(int fd) {
-  ioctl(fd, PERF_EVENT_IOC_RESET, 0);
-  ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
-}
+void m_hwmisses() {
+  struct perf_event_attr pe;
+  long long val1_1, val1_2, val1_3, val1_4;
+  int fd1_1, fd1_2, fd1_3, fd1_4;
 
-void turnoff_measuring(int fd) {
-  ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
+  /** Cache misses **/
+  memset(&pe, 0, sizeof(struct perf_event_attr));
+  pe.type = PERF_TYPE_HARDWARE;
+  pe.size = sizeof(struct perf_event_attr);
+  pe.config = PERF_COUNT_HW_CACHE_MISSES;
+  pe.disabled = 1;
+  pe.exclude_kernel = 1;
+  pe.exclude_hv = 1;
+
+  // have to create a file descriptor for each core 
+  // Raspberry Pi 3B+
+  fd1_1 = perf_event_open(&pe, -1, 0, -1, 0);
+  fd1_2 = perf_event_open(&pe, -1, 1, -1, 0);
+  fd1_3 = perf_event_open(&pe, -1, 2, -1, 0);
+  fd1_4 = perf_event_open(&pe, -1, 3, -1, 0);
+  
+  if (fd1_1 == -1) {
+    fprintf(stderr, "Error opening leader %llx\n", pe.config);
+  }
+  if (fd1_2 == -1) {
+    fprintf(stderr, "Error opening leader %llx\n", pe.config);
+  }
+  if (fd1_3 == -1) {
+    fprintf(stderr, "Error opening leader %llx\n", pe.config);
+  }
+  if (fd1_4 == -1) {
+    fprintf(stderr, "Error opening leader %llx\n", pe.config);
+  }
+
+  /** Branch misses **/
+  long long val2_1, val2_2, val2_3, val2_4;
+  int fd2_1, fd2_2, fd2_3, fd2_4;
+
+  memset(&pe, 0, sizeof(struct perf_event_attr));
+  pe.type = PERF_TYPE_HARDWARE;
+  pe.size = sizeof(struct perf_event_attr);
+  pe.config = PERF_COUNT_HW_CACHE_MISSES;
+  pe.disabled = 1;
+  pe.exclude_kernel = 1;
+  pe.exclude_hv = 1;
+
+  // have to create a file descriptor for each core 
+  // Raspberry Pi 3B+
+  fd2_1 = perf_event_open(&pe, -1, 0, fd1_1, 0);
+  fd2_2 = perf_event_open(&pe, -1, 1, fd1_2, 0);
+  fd2_3 = perf_event_open(&pe, -1, 2, fd1_3, 0);
+  fd2_4 = perf_event_open(&pe, -1, 3, fd1_4, 0);
+
+
+  // Begin testing
+  turnon_measuring(fd1_1);
+  turnon_measuring(fd1_2);
+  turnon_measuring(fd1_3);
+  turnon_measuring(fd1_4);
+  
+  sleep(5);
+
+  turnoff_measuring(fd1_1);
+  turnoff_measuring(fd1_2);
+  turnoff_measuring(fd1_3);
+  turnoff_measuring(fd1_4);
+  
+  read(fd1_1, &val1_1, sizeof(long long));
+  read(fd1_2, &val1_2, sizeof(long long));
+  read(fd1_3, &val1_3, sizeof(long long));
+  read(fd1_3, &val1_4, sizeof(long long));
+  
+  read(fd2_3, &val2_4, sizeof(long long));
+  read(fd2_3, &val2_4, sizeof(long long));
+  read(fd2_3, &val2_4, sizeof(long long));
+  read(fd2_3, &val2_4, sizeof(long long));
+
+  printf("\nHardware misses:\n");
+  printf("\tCPU0: %lld cache misses\n", val1_1);
+  printf("\tCPU1: %lld cache misses\n", val1_2);
+  printf("\tCPU2: %lld cache misses\n", val1_3);
+  printf("\tCPU3: %lld cache misses\n\n", val1_4);
+
+  printf("\tCPU0: %lld branch misses\n", val1_4);
+  printf("\tCPU1: %lld branch misses\n", val1_4);
+  printf("\tCPU2: %lld branch misses\n", val1_4);
+  printf("\tCPU3: %lld branch misses\n", val1_4);
+  
+  close(fd1_1);
+  close(fd1_2);
+  close(fd1_3);
+  close(fd1_4);
+
+  close(fd2_1);
+  close(fd2_2);
+  close(fd2_3);
+  close(fd2_4);
 }
